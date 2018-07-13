9977503dfa0b59e2310641a2dad5ee77d87327dd
Krut Patel <krut@Kruts-MacBook-Pro.local>
2018-07-13 15:59:48 +0530

    prototype

---
+++ Assets/LevelGeneratorScript.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGeneratorScript : MonoBehaviour {
+
+	public static LevelGeneratorScript instance;
+
+	public GameObject ring;
+	public GameObject spikePrefab;
+	public GameObject rodPrefab;
+	public Vector3 spikePosition;
+	public Vector3 ringPosition;
+	public Vector3 rodPosition;
+
+	private List<GameObject> levelObjects = new List<GameObject>();
+
+	void Awake(){
+		instance = this;
+	}
+
+	void Start(){
+		GenerateLevel();
+	}
+
+	public void GenerateLevel(){
+		foreach (GameObject spike in levelObjects)
+		{
+			Destroy(spike);			
+		}
+		levelObjects.Clear();
+		CreateSpike();
+		SetRing();
+	}
+
+	public void CreateSpike(){
+		GameObject spike = Instantiate(spikePrefab);
+		levelObjects.Add(spike);
+		switch(Random.Range(0,4)){
+			case 0: // top 
+				spike.transform.position = new Vector3(
+											Random.Range(-spikePosition.x, spikePosition.x),
+											spikePosition.y, 
+											0);
+				spike.transform.rotation = Quaternion.Euler(0, 0, -90);
+				break;
+			case 1: // bot 
+				spike.transform.position = new Vector3(
+											Random.Range(-spikePosition.x, spikePosition.x),
+											-spikePosition.y, 
+											0);
+				spike.transform.rotation = Quaternion.Euler(0, 0, 90);
+				break;
+			case 2: // left
+				spike.transform.position = new Vector3(
+											-spikePosition.x,
+											Random.Range(-spikePosition.y, spikePosition.y),
+											0);
+				spike.transform.rotation = Quaternion.Euler(0, 0, 0);
+				break;
+			case 3: // right
+				spike.transform.position = new Vector3(
+											spikePosition.x,
+											Random.Range(-spikePosition.y, spikePosition.y),
+											0);
+				spike.transform.rotation = Quaternion.Euler(0, 0, 180);
+				break;
+		}
+
+		if(Random.Range(0,5) <= 2){
+			GameObject rod = Instantiate(rodPrefab, new Vector3(rodPosition.x, Random.Range(-rodPosition.y, rodPosition.y), 0), rodPrefab.transform.rotation);
+			levelObjects.Add(rod);
+		}
+	}
+
+	public void SetRing(){
+		ring.SetActive(false);
+		ring.transform.position = new Vector3(
+			Random.Range(-ringPosition.x, ringPosition.x),
+			Random.Range(-ringPosition.y, ringPosition.y),
+			0);
+		ring.transform.rotation = Quaternion.identity;
+		ring.SetActive(true);
+	}
+}
---
+++ Assets/PlayerControllerScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerScript : MonoBehaviour {
+	
+	public float force;
+
+	private Rigidbody2D rb;
+	private Vector3[] directions;
+	private int directionPointer;
+
+	void Start(){
+		rb = GetComponent<Rigidbody2D>();
+		directions = new Vector3[2];
+		directions[0] = new Vector3(1, 2, 0);
+		directions[1] = new Vector3(-1, 2, 0);
+
+		directionPointer = 0;
+	}
+
+	void Update () {
+		if(Input.GetMouseButtonDown(0)){
+			rb.velocity = Vector3.zero;
+			rb.AddForce(directions[directionPointer] * force);
+
+			directionPointer++;
+			if(directionPointer > 1){
+				directionPointer = 0;
+			}
+		}		
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Ring")){
+			StartCoroutine(WaitBeforeGeneratingLevel());
+		}
+    }
+
+	IEnumerator WaitBeforeGeneratingLevel(){
+		yield return new WaitForSeconds(1);
+		LevelGeneratorScript.instance.GenerateLevel();
+	}
+}
